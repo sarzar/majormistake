3dec91ab254de3f76c2290d5db28edc120d36727
hamzahrawasia <hamzah.rawasia@gmail.com>
2022-01-15 18:20:35 -0500

    adjusted how to play page

--- majormistakegame/src/components/Howtoplay.js
+++ majormistakegame/src/components/Howtoplay.js
@@ -6,18 +6,20 @@ export default function Howtoplay() {
     return (
         <div className="rules">
 
-            <h1>How to Play:</h1>
-            <ul className="list">
+            <h1 className="howtoplay-title">How to Play:</h1>
+            <ul>
+                <div className="box">
                 <li>
                     Two players go head to head, asking each other a series of questions 
                     in order to guess each other's majors!
-                </li>
+                </li> <br/>
                 <li>
                     Players will be assigned a bank of questions they are able to ask, up to a maximum of 10
-                </li>
+                </li> <br/>
                 <li>Player's have 1 opportunity to guess the other player's major at any point.
                     If you guess correcty, you win! If not, you lose.  
                 </li>
+                </div>
             </ul>
         </div>
     );

4eb2957418ca08f770d8cb5205704eb528f43dd8
Sarim Zia <username@email.com>
2022-01-15 14:37:39 -0500

    Set up react format

---
+++ majormistakegame/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css"
+import '../App.css';
+
+export default function Home() {
+    return (
+        <div className="Homepage">
+
+            <h1>HI</h1>
+        </div>
+    );
+}

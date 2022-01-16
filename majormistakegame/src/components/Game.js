faba0fe46e1d4a8e1913fe99f802f34e19e2a3b9
Sarim Zia <username@email.com>
2022-01-15 22:41:29 -0500

    '

--- majormistakegame/src/components/Game.js
+++ majormistakegame/src/components/Game.js
@@ -1,13 +1,39 @@
 import React from "react";
 import "./Game.css";
-import '../App.css';
+
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import Popup from './Popup';
+
 
 export default function Game() {
-
+    const [popupe, setPopupe] = useState(false);
     return (
-<div className = "game">
-    
-</div>
+
+        <div className="game-container">
+            <div className="wingiveupbuttons-container">
+                <Link to="/">
+                    <button id="giveup" className="creategameimage"> <img src="resizedImage (14).png" /></button>
+                </Link>
+                <div className="spacer"></div>
+
+                <button className="creategameimage" onClick={() => setPopupe(true)} onClick={() => setPopupe(true)} variant="outlined" ><img src='resizedImage (15).png' /></button>
+                <div className="popup-content">
+                    <Popup trigger={popupe} setTrigger={setPopupe}>
+                        <h3>GUESS</h3>
+                        <p>Code: XGTSR</p>
+                        <Link to="gamepage">
+                            <button>Create</button>
+                        </Link>
+                    </Popup>
+                </div>
+
+
+            </div>
+            <div className="Question-container">
+                <img src="art.png" />
+            </div>
+        </div>
 
 
     );

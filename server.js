85d122549032f65f40a8999ea967b04d6279cbb0
Mohammad Mahdi Mahboob <85646892+habibiLaFerrari97@users.noreply.github.com>
2022-01-16 10:46:12 -0500

    testing socket.io

---
+++ server.js
@@ -0,0 +1,16 @@
+﻿const express = require('express');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+const socket = require('socket.io');
+const io = socket(server);
+
+
+io.on("connection", socket => {
+    socket.emit('userID', socket.id);
+    socket.onabort('send message', body => {
+        io.emit('message', body);
+    });
+});
+
+server.listen(3000, () => console.log('listening to *:3000'))
